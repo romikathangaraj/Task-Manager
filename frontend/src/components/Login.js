48ad09f65d1d1c56168067549d3fda3664184080
Romika <144676067+romikathangaraj@users.noreply.github.com>
2025-01-07 14:42:56 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", {
+      const response = await axios.post("https://task-manager-backendservice.onrender.com/login", {
         email,
         password,
       });
